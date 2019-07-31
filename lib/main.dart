85483b45944f88d7d81018e9ee2aa7f511391409
Raulivan <raulivanrodrigo@yahoo.com.br>
2019-07-31 12:49:07 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:app_gifss/view/home_page.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: HomePage() ,
+    theme: ThemeData(hintColor: Colors.white ) ,
+  ));
+}
+
